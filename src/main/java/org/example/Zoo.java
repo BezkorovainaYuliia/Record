2d8566be74b2d2ff85b5c31ee1a7be5f22870d23
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-07-08 11:07:58 +0200

    Records

---
+++ src/main/java/org/example/Zoo.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.List;
+
+public record Zoo(List<Animal> animals) {
+    private final static String NAME_ZOO = "Hagen Tier Hamburg";
+    public int calculateFood() {
+        int food = 0;
+        for (Animal animal : animals) {
+            System.out.println(animal.name() + " eats " + animal.foodProDay().foodProDay() + " gram of food");
+            food += animal.foodProDay().foodProDay();
+        }
+        return food;
+    }
+
+    public void getNameZoo() {
+        System.out.println("Welkom to " + NAME_ZOO);
+    }
+
+    public Zoo withAnimal(Animal newAnimal){
+        if (newAnimal == null) throw new IllegalArgumentException("animal cannot be null");
+        animals.add(newAnimal);
+        return new Zoo(animals);
+    }
+}
